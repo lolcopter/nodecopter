aeaad873df25d121ac6370e5a7fde23f852e37b9
Lauri Piispanen <lauri.piispanen@reaktor.fi>
2013-04-20 11:33:09 +0300

    bacon copter that stays between 1 and 2 meters

--- baconcopter.js
+++ baconcopter.js
@@ -2,22 +2,58 @@ var _       = require('underscore'),
     Bacon   = require('baconjs'),
     arDrone = require('ar-drone'),
     client  = arDrone.createClient({ip: '192.168.1.1'}),
-    navdata = new Bacon.Bus();
-
-client.on("navdata", navdata.push);
-client.config('general:navdata_demo', 'FALSE');
-
-navdata.onValue(_.bind(console.log, console));
-
+    navdata = new Bacon.Bus(),
+    navdataclean = navdata.map(function(it) {
+      if (!it.demo) {
+        return null;
+      } else {
+        return it;
+      }
+    }).filter(function(it) { return it != null }),
+    altitude = navdataclean.map(function(it) {
+      return it.demo.altitude
+    }).skipDuplicates(),
+    battery = navdataclean.map(function(it) {
+      return it.demo.batteryPercentage;
+    }).skipDuplicates()
+    toolow  = altitude.map(function(it) {
+      return it < 1;
+    }).skipDuplicates();
+    toohigh  = altitude.map(function(it) {
+      return it > 2;
+    }).skipDuplicates(),
+    landing = false;
 
 // try flying
 client.disableEmergency();
+
+client.config('general:navdata_demo', 'FALSE');
+client.on("navdata", navdata.push);
+
 client.takeoff();
 
-client.after(3000, function() {
-    this.clockwise(0.5);
-  })
-  .after(3000, function() {
+toolow.filter(function(it) {
+      return it;
+    }).onValue(function() {
+  console.log("toolow");
+  if (!landing) {
+    client.up(1);
+  }
+});
+battery.map(function(it) { return "battery: " + it + "%" }). log();
+
+toohigh.filter(function(it) {
+      return it;
+}).onValue(function() {
+  console.log("toohigh");
+  if (!landing) {
+    client.down(1);
+  }
+});
+
+client
+  .after(20000, function() {
+    landing = true;
     this.stop();
     this.land();
   });
